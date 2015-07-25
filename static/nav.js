63fe5dcf5b3a5646590e8ca349c0c9631f834bbb
Edward Delaporte <edthedev@gmail.com>
2015-07-25 18:42:52 -0500

    Shared navigation!

--- static/nav.js
+++ static/nav.js
@@ -1,1 +1,4 @@
-$( ".nav" ).append( "<p>NAV GOES HERE!</p>" );
+$( ".nav" ).append( "<ul>");
+$( ".nav" ).append( "<li><a href='index.html'>Welcome</a></li>" );
+$( ".nav" ).append( "<li><a href='ultisnips.html'>On Ultisnips</a></li>" );
+$( ".nav" ).append( "</ul>");
