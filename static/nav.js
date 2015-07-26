958b0ac7620fc9730e9e18687e0a966e48175497
Edward Delaporte <edthedev@gmail.com>
2015-07-25 22:14:09 -0500

    Split list up a bit.

--- static/nav.js
+++ static/nav.js
@@ -1,4 +1,6 @@
 $( ".nav" ).append( "<ul>");
 $( ".nav" ).append( "<li><a href='index.html'>Welcome</a></li>" );
+$( ".nav" ).append( "<label for=recent>Recent Posts</label><ul id=recent>");
 $( ".nav" ).append( "<li><a href='ultisnips.html'>On Ultisnips</a></li>" );
+$( ".nav" ).append( "</ul>");
 $( ".nav" ).append( "</ul>");
