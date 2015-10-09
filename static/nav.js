443930624e4c1ae74c949c74f140b2c122f9d511
Edward Delaporte <edthedev@gmail.com>
2015-10-08 22:29:53 -0500

    Much nicer. Nav fixed.

--- static/nav.js
+++ static/nav.js
@@ -1,4 +1,5 @@
-$( "#navlist" ).append( "<label for=recent>Navigation</label><ul id=navigation>");
+$( "#navlist" ).append( "<label for=navigation>Navigation</label><ul id=navigation>");
 $( "#navlist" ).append( "<lrecenti><a href='index.html'>Welcome</a></li>" );
-$( "#navlist" ).append( "<li><a href='ultisnips.html'>On Ultisnips</a></li>" );
+$( "#navlist" ).append( "<li><a href='fishbowl.html'>Fish Bowl</a></li>" );
+/* $( "#navlist" ).append( "<li><a href='ultisnips.html'>Ultisnips</a></li>" ); */
 $( "#navlist" ).append( "</ul>");
