37a347cbd4d9bf7e98e308720b595a05d56c6c9c
Edward Delaporte <edthedev@gmail.com>
2015-10-08 22:48:32 -0500

    Added TFS Notification script.

--- static/nav.js
+++ static/nav.js
@@ -1,5 +1,6 @@
 $( "#navlist" ).append( "<label for=navigation>Navigation</label><ul id=navigation>");
-$( "#navlist" ).append( "<lrecenti><a href='index.html'>Welcome</a></li>" );
+$( "#navlist" ).append( "<li><a href='index.html'>Welcome</a></li>" );
+$( "#navlist" ).append( "<li><a href='tfsNotify.html'>TFS Notification</a></li>" );
 $( "#navlist" ).append( "<li><a href='fishbowl.html'>Fish Bowl</a></li>" );
 /* $( "#navlist" ).append( "<li><a href='ultisnips.html'>Ultisnips</a></li>" ); */
 $( "#navlist" ).append( "</ul>");
