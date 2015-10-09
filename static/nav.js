128ba6ef4fd36941a432172173eeee80934836f3
Edward Delaporte <edthedev@gmail.com>
2015-10-08 22:14:52 -0500

    Trying to improve the colors.

--- static/nav.js
+++ static/nav.js
@@ -1,6 +1,4 @@
-$( ".nav" ).append( "<ul>");
-$( ".nav" ).append( "<li><a href='index.html'>Welcome</a></li>" );
-$( ".nav" ).append( "<label for=recent>Recent Posts</label><ul id=recent>");
-$( ".nav" ).append( "<li><a href='ultisnips.html'>On Ultisnips</a></li>" );
-$( ".nav" ).append( "</ul>");
-$( ".nav" ).append( "</ul>");
+$( "#navlist" ).append( "<label for=recent>Navigation</label><ul id=navigation>");
+$( "#navlist" ).append( "<lrecenti><a href='index.html'>Welcome</a></li>" );
+$( "#navlist" ).append( "<li><a href='ultisnips.html'>On Ultisnips</a></li>" );
+$( "#navlist" ).append( "</ul>");
