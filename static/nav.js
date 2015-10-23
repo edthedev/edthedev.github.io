b197a4edb894cd05305e516865c37562a027b958
Edward Delaporte <edthedev@gmail.com>
2015-10-22 22:30:08 -0500

    Such gallery wow.

--- static/nav.js
+++ static/nav.js
@@ -1,6 +1,7 @@
 $( "#navlist" ).append( "<label for=navigation>Navigation</label><ul id=navigation>");
 $( "#navlist" ).append( "<li><a href='index.html'>Welcome</a></li>" );
 $( "#navlist" ).append( "<li><a href='tfsNotify.html'>TFS Notification</a></li>" );
+$( "#navlist" ).append( "<li><a href='gallery.html'>Art Gallery</a></li>" );
 $( "#navlist" ).append( "<li><a href='fishbowl.html'>Fish Bowl</a></li>" );
 /* $( "#navlist" ).append( "<li><a href='ultisnips.html'>Ultisnips</a></li>" ); */
 $( "#navlist" ).append( "</ul>");
