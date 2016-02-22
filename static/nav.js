08f79bdd8a996aaf070edac9a4e5e7e5479cb458
Delaporte, Edward <Edward.Delaporte@busey.com>
2016-02-22 12:44:01 -0600

    Some nav updates.

--- static/nav.js
+++ static/nav.js
@@ -3,5 +3,7 @@ $( "#navlist" ).append( "<li><a href='index.html'>Welcome</a></li>" );
 $( "#navlist" ).append( "<li><a href='gallery.html'>Art Gallery</a></li>" );
 $( "#navlist" ).append( "<li><a href='fishbowl.html'>Fish Bowl</a></li>" );
 $( "#navlist" ).append( "<li><a href='tfsNotify.html'>TFS Notification Script</a></li>" );
+$( "#navlist" ).append( "<li><a href='VisualStudio.html'>Visual Studio with Git</a></li>" );
+$( "#navlist" ).append( "<li><a href='WebCon2016.html'>WebCon 2016 Presentation</a></li>" );
 /* $( "#navlist" ).append( "<li><a href='ultisnips.html'>Ultisnips</a></li>" ); */
 $( "#navlist" ).append( "</ul>");
