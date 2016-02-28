af28c61d4863c83a1992e59d6ccb25e0364db9d9
Edward Delaporte <edthedev@gmail.com>
2016-02-28 11:33:06 -0600

    Link to resume.

--- static/nav.js
+++ static/nav.js
@@ -4,6 +4,7 @@ $( "#navlist" ).append( "<li><a href='gallery.html'>Art Gallery</a></li>" );
 $( "#navlist" ).append( "<li><a href='projects.html'>Code</a></li>" );
 $( "#navlist" ).append( "<li><a href='solutions.html'>Solutions</a></li>" );
 $( "#navlist" ).append( "<li><a href='fishbowl.html'>Fish Bowl</a></li>" );
+$( "#navlist" ).append( "<li><a href='resume.html'>Resume</a></li>" );
 $( "#navlist" ).append( "<li><a href='WebCon2016.html'>WebCon 2016 Presentation</a></li>" );
 $( "#navlist" ).append( "</ul>");
 $( "#navlist" ).append( "<br/>");
