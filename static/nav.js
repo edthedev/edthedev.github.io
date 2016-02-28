c2686eeec787fb6c1bebcd89e8ae54545b2d7f77
Edward Delaporte <edthedev@gmail.com>
2016-02-27 21:01:35 -0600

    Tightened up the navigation a bit.

--- static/nav.js
+++ static/nav.js
@@ -1,10 +1,9 @@
 $( "#navlist" ).append( "<label for=navigation>Navigation</label><ul id=navigation>");
 $( "#navlist" ).append( "<li><a href='index.html'>Welcome</a></li>" );
 $( "#navlist" ).append( "<li><a href='gallery.html'>Art Gallery</a></li>" );
+$( "#navlist" ).append( "<li><a href='projects.html'>Code</a></li>" );
+$( "#navlist" ).append( "<li><a href='solutions.html'>Solutions</a></li>" );
 $( "#navlist" ).append( "<li><a href='fishbowl.html'>Fish Bowl</a></li>" );
-$( "#navlist" ).append( "<li><a href='tfsNotify.html'>TFS Notification Script</a></li>" );
-$( "#navlist" ).append( "<li><a href='VisualStudio.html'>Visual Studio with Git</a></li>" );
 $( "#navlist" ).append( "<li><a href='WebCon2016.html'>WebCon 2016 Presentation</a></li>" );
-/* $( "#navlist" ).append( "<li><a href='ultisnips.html'>Ultisnips</a></li>" ); */
 $( "#navlist" ).append( "</ul>");
 $( "#navlist" ).append( "<br/>");
