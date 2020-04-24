0e95b4136f4771a75763a6fdba9529034dfac2cc
Edward Delaporte <delaport@illinois.edu>
2020-04-24 16:29:37 -0500

    Move the JS into shared location.

--- js/nav.js
+++ js/nav.js
@@ -1,3 +1,7 @@
-$.get("nav.html", function (html) {
-    $("#navlist").html(html);
+$( document ).ready( function() {
+    // Overwrite ye_navbar with shared nav bar.
+    // Wastes CPU cycles, but so does everything on my site.
+    $.get('/nav/').done( function( data ) { 
+        $(".ye_navbar").html(data);
+    } );
 });
