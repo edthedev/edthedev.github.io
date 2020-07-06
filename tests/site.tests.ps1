75c98c2c69277bd000d6a774da7deef33e60d13d
Edward Delaporte <delaport@illinois.edu>
2020-07-06 13:51:33 -0500

    Test for site title.
    Also add a site title.

--- tests/site.tests.ps1
+++ tests/site.tests.ps1
@@ -1,8 +1,22 @@
+# Test locally with:
+# (Be sure to also update to PowerShell 7)
+# As Admin:
+# > Install-Module -Name Pester -Force -SkipPublisherCheck
+# then not as admin:
+# > Invoke-Pester ./tests
+
 Describe 'Site Content Tests' {
     It 'has an index file' {
-      Test-Path -Path ./index.html | Should Be $true
+      Test-Path -Path ./index.html | Should -Be $true
     }
-    It 'has a penguin file' {
-      Test-Path -Path ./penguins_are_cool.html | Should Be $true
+    It 'has no penguin file' {
+      Test-Path -Path ./penguins_are_cool.html | Should -Be $false
     }
+    $index_content = Get-Content ./index.html
+
+    It 'has the correct page title' {
+      $expected_title = "<title>Edward.Delaporte.us</title>"
+      '.\index.html' | Should -FileContentMatch $expected_title
+    }
+
 }
