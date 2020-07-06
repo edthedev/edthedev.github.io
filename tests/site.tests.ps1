5009274c8c6306c63d4829c87e742f96ff6c80a8
Edward Delaporte <delaport@illinois.edu>
2020-07-06 13:33:11 -0500

    Let's fail a test.

--- tests/site.tests.ps1
+++ tests/site.tests.ps1
@@ -2,4 +2,7 @@ Describe 'Site Content Tests' {
     It 'has an index file' {
       Test-Path -Path ./index.html
     }
+    It 'has a penguin file' {
+      Test-Path -Path ./penguins_are_cool.html
+    }
 }
