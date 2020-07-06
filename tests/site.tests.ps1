3ec280c5c6d9e6c5fe3d4660de7337d2a94618ce
Edward Delaporte <delaport@illinois.edu>
2020-07-06 13:40:19 -0500

    Let's fail at something.

--- tests/site.tests.ps1
+++ tests/site.tests.ps1
@@ -1,8 +1,8 @@
 Describe 'Site Content Tests' {
     It 'has an index file' {
-      Test-Path -Path ./index.html
+      Test-Path -Path ./index.html | Should Be $true
     }
     It 'has a penguin file' {
-      Test-Path -Path ./penguins_are_cool.html
+      Test-Path -Path ./penguins_are_cool.html | Should Be $true
     }
 }
