49b6cddc7b1faf90e3cc6a2d50a84872e9a46483
EdTheDev <edward@delaporte.us>
2021-04-11 16:35:35 -0500

    Neat trick.

---
+++ art/live/cross.js
@@ -0,0 +1,70 @@
+
+/*
+
+"Cross" is a Live Art work created by Edward Delaporte.
+
+This script is Copyright Edward Delaporte 2021.
+
+This script and the art it creates are licensed under 
+a Creative Commons Attribution-ShareAlike 4.0 
+International License.
+
+http://creativecommons.org/licenses/by-sa/4.0/
+
+You can share your own remix of this code 
+as long as you display this license and attribution.
+
+*/
+
+var ball = {
+  x: 0,
+  y: 0,
+  dx: 1,
+  dy: 1,
+  size: 20,
+  bouncex: 50,
+  bouncey: 50
+};
+
+function setup() {
+  // maxim = .5 * window.innderWidth;
+  maxim = 400;
+  midline = maxim / 2;
+
+  createCanvas(maxim, maxim);
+  background(maxim,maxim);
+  balls = [];
+  ballcount = random(3,15);
+
+  theball = Object(ball);
+  for(i=0;i<ballcount;i++){
+    newb = Object.assign({}, theball);
+    maxline = random(maxim/2, maxim);
+    minline = random(0, maxim/2);
+    newb.x = random(0,maxline);
+    newb.y = i * 22;
+    newb.bouncex = maxline;
+    newb.bouncey = maxline;
+    balls.push(newb);
+  }
+}
+
+function doBall(ball) {
+  ball.x += ball.dx;
+  ball.y += ball.dy;
+
+  if(ball.x>ball.bouncex) ball.dx = -1;
+  if(ball.y>maxim) ball.dy = -1;
+  if(ball.x<0) ball.dx = 1;
+  if(ball.y<0) ball.dy = 1;
+  circle(ball.x, ball.y, ball.size);
+  return ball;
+}
+
+function draw() {
+  balls = balls.map( doBall );
+}
+
+function mouseClicked() {
+  balls.forEach( ball => ball.bouncex = mouseX );
+}
