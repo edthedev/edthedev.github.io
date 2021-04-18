2585cb90d3a311a1bd44cfb51f38a9c860d35e54
EdTheDev <edward@delaporte.us>
2021-04-18 13:47:40 -0500

    Add star field to sketch 1

--- art/live/sketch1.js
+++ art/live/sketch1.js
@@ -15,18 +15,40 @@ as long as you display this license and attribution.
 
 */
 
+function starfield() {
+  // White start on black backgroud
+  background('#222222');
+  fill(color(255,255,255));
+  
+  // Randomly scatter stars.
+  starcount = random(20,60);
+  for(i=0;i<starcount;i++){
+    starx = random(0, maxim);
+    stary = random(0, maxim);
+    circle(starx, stary, 4);
+  }
+}
+
+
+
 function cornerstroke(sx1, sy1, sx2, sy2) {
   line(sx1, sy1, sx2, sy2/2);
   line(sx2, sy2/2, sx1, sy2);
 }
+
+// var colorshade = 0;
 
 function setup() {
   maxim = 400;
   midline = maxim / 2;
   createCanvas(maxim, maxim);
   background(maxim,maxim);
+  starfield();
   cornercount = random(3,10);
   for(i=0;i<cornercount;i++){
+    // colorshade+=40;
+    let c = color(random(0,10)*25,random(0,10)*25, random(0,10)*25);
+    fill(c);
     // cornerstroke(0,0,50 * i,50*i+30);
     triangle(0,0, 50*i, 50*i+30, maxim/i, 0);
   }
