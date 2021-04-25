f19788a17c3f3a1bfcf46be1f604183cf07d39b9
EdTheDev <edward@delaporte.us>
2021-04-25 15:32:09 -0500

    Use ev ignore

--- .eleventy.js
+++ .eleventy.js
@@ -4,11 +4,16 @@ module.exports = eleventyConfig => {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPassthroughCopy("slides");
 
   eleventyConfig.setTemplateFormats([
     "md",
     "njk"
   ]);
+
+  eleventyConfig.setQuietMode(true);
+  eleventyConfig.setWatchThrottleWaitTime(500); // in milliseconds
+  eleventyConfig.setUseGitIgnore(false);
 
   return {
     dir: {
