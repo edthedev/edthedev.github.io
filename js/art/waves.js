dedd7472d1272116e07a9f6eb3059baf1df248af
Edward Delaporte <delaport@illinois.edu>
2021-05-04 13:48:35 -0500

    click to reset

--- js/art/waves.js
+++ js/art/waves.js
@@ -122,27 +122,7 @@ function draw() {
 }
 
 function mouseClicked() {
-  // background(220);
-  tree(mouseX, mouseY, 20)
-  // spinny(mouseX, mouseY, 20)
-}
-
-function tree(x, y, size, ang=1) {
-  var c = color(0, random(200, 255), random(100, 200));
-  fill(c);
-  delta = size * .5
-  if(size % 4 == 0){
-    deltax = random(delta,delta*2)
-    newang = random([-1,1]) 
-    newx = x - deltax
-
-    tree(newx,y,size-1, newang)
-  }
-  if(size>4) {
-    circle(x, y, size)
-    deltax = random(0,delta*2)
-    newx = x - ang * deltax
-    newy = y - size * .5
-    tree(newx,newy,size-1)
-  }
+  fillcount = 0;
+  frameRate(300);
+  setup();
 }
