bc9397dd033e0efe097a723af5440be6936df917
EdTheDev <edward@delaporte.us>
2021-05-13 16:51:36 -0500

    One TOS solution - lacks links.

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
 module.exports = eleventyConfig => {
 
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("js");
