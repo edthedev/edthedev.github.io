8c72bd614549faca9f2c449050b306c8234eccfc
EdTheDev <edward@delaporte.us>
2021-06-18 08:58:02 -0500

    Starting a new weave.

--- js/art/latest.js
+++ js/art/latest.js
@@ -2,7 +2,7 @@
 
 This is a Live Art work created by Edward Delaporte.
 
-This script is Copyright Edward Delaporte 2020.
+This script is Copyright Edward Delaporte 2021.
 
 This script and the art it creates are licensed under 
 a Creative Commons Attribution-ShareAlike 4.0 
@@ -13,75 +13,50 @@ http://creativecommons.org/licenses/by-sa/4.0/
 You can share your own remix of this code 
 as long as you display this license and attribution.
 
-
-+ [ ] add grass/ground around base of each tree.
-+ [ ] get rid of ultra short branches nearest to trunk
-
 */
 
-let ani_stack = [];
-let ani_stack_max = 30;
 
-function ani_plan(item) {
-  if(ani_stack.length < ani_stack_max) {
-    ani_stack.push(item);
-  }
-  // console.debug(ani_stack.length);
+var weave = {
+  x: 0,
+  y: 0,
+  dx: 1,
+  dy: 1,
+  rulefun: dewit,
+  size: 20,
+  color = color(0, 255,0);
+};
+
+function draw_weave(weave) {
+  fill(weave.color);
+  rect(x,y, 20);
 }
-
-function ani_clear() {
-  while(ani_stack.length > 0) {
-    ani_stack.pop();
-  }
-}
-
-function ani_do() {
-  ani_speed = 4;
-  for(ani_i = 0; ani_i < ani_speed; ani_i++) {
-    if(ani_stack.length > 0) {
-      anim = ani_stack.pop();
-      anim();
-    }
-  }
+function anim_weave(weave) { 
+  weave.x += dx;
+  weave.y += dy;
+  return weave;
 }
 
 function setup() {
   ani_clear();
-  // maxim = .5 * window.innderWidth;
-  maxim_x = 800;
-  maxim_y = 400;
-  midline = maxim_x / 2;
-  myCanvas = createCanvas(maxim_x, maxim_y);
-  fill(color(230,230,230));
 
-  make_horizon(0, 0, maxim_x, maxim_y);
-  draw_horizon(0, 0, maxim_x, maxim_y);
+  balls = [];
+  ballcount = random(3,5);
 
-  setup_tree();
-
-  draw_ground(myCanvas);
-
-  num_trees = choose([2, 3, 4, 5]);
-  for(tree_count=0; tree_count<num_trees; tree_count++) {
-    ani_plan(
-      item = 
-      function() { 
-
-    trunk(
-      random(0, maxim_x),
-      7/8*maxim_y,
-      3,
-      60
-    );
-
-      }
-    )
+  theball = Object(weave);
+  for(i=0;i<ballcount;i++){
+    newb = Object.assign({}, theball);
+    maxline = random(maxim/2, maxim);
+    minline = random(0, maxim/2);
+    newb.x = random(0,maxline);
+    newb.y = i * 22;
+    balls.push([newb, dewit]);
   }
+
 
 }
 
 function draw() {
-  ani_do();
+  balls = balls.map( item => item[1](item[0]) );
 }
 
 function mouseClicked() {
---
+++ js/art/share/canvas.js
@@ -0,0 +1,10 @@
+
+function setup_canvas() {
+
+  maxim_x = 800;
+  maxim_y = 400;
+  midline = maxim_x / 2;
+  myCanvas = createCanvas(maxim_x, maxim_y);
+  fill(color(230,230,230));
+
+}
