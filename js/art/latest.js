502694de34918559de537c786dc7bc2962c2f4a4
EdTheDev <edward@delaporte.us>
2021-06-18 22:21:52 -0500

    Make some wormy paths.

--- js/art/latest.js
+++ js/art/latest.js
@@ -21,42 +21,75 @@ var weave = {
   y: 0,
   dx: 1,
   dy: 1,
-  rulefun: dewit,
+  sequence: [],
   size: 20,
-  color = color(0, 255,0);
+  color: null
 };
+
+var pulse = 0;
+var beat = 30;
 
 function draw_weave(weave) {
   fill(weave.color);
-  rect(x,y, 20);
-}
-function anim_weave(weave) { 
-  weave.x += dx;
-  weave.y += dy;
+  circle(weave.x,weave.y, weave.size);
   return weave;
 }
 
+function anim_weave(weave) { 
+  weave.x += weave.dx;
+  weave.y += weave.dy;
+  return weave;
+}
+
+function fresh_color() {
+  let c = color(random(0,10)*30,random(0,10)*30, random(0,10)*30);
+  return c;
+}
+
 function setup() {
-  ani_clear();
 
+  setup_canvas();
+
+  maxline = 400;
   balls = [];
-  ballcount = random(3,5);
+  item_count = random(3,5);
 
-  theball = Object(weave);
-  for(i=0;i<ballcount;i++){
-    newb = Object.assign({}, theball);
-    maxline = random(maxim/2, maxim);
-    minline = random(0, maxim/2);
+  weave_template = Object(weave);
+  for(i=0;i<item_count;i++){
+    newb = Object.assign({}, weave_template);
     newb.x = random(0,maxline);
-    newb.y = i * 22;
-    balls.push([newb, dewit]);
-  }
+    newb.y = i * 22; // Make sure they each start offset a bit
+    newb.color = fresh_color();
 
+    base_path = [1,1, 1,-1, -1,-1, -1, 1];
+    newb.sequence = base_path.map( item => item * choose([1,2,3]));
+
+    console.debug(newb);
+    balls.push(newb);
+  }
+  noStroke();
 
 }
 
+function shift_weave(item) {
+  if(item.sequence.length > 0) {
+    item.dx = item.sequence.shift();
+    item.dy = item.sequence.shift();
+  } else {
+    item.dx = 0;
+    item.dy = 0;
+  }
+  return item;
+}
+
 function draw() {
-  balls = balls.map( item => item[1](item[0]) );
+  pulse++;
+  if(pulse > beat) {
+    pulse = 0;
+    balls = balls.map( item => shift_weave(item) );
+  }
+  balls = balls.map( item => anim_weave(item) );
+  balls = balls.map( item => draw_weave(item) );
 }
 
 function mouseClicked() {
