cba2e9d88c66a821535a4481915820e5f136fa7f
EdTheDev <edward@delaporte.us>
2021-07-11 11:27:18 -0500

    Reset for next sketch.

--- js/art/latest.js
+++ js/art/latest.js
@@ -16,7 +16,7 @@ as long as you display this license and attribution.
 */
 
 
-var weave = {
+var item_template = {
   x: 0,
   y: 0,
   dx: 1,
@@ -27,97 +27,18 @@ var weave = {
   color2: null
 };
 
+var items = [];
 var pulse = 0;
 var beat = 25;
 
-function draw_weave(weave) {
-  // stroke(color(0,0,0)); // black outline
-  fill(weave.color2);
-
-  edge_dx = .5 * weave.size;
-
-  if(weave_type == "cross") {
-
-    // large background square
-    rect(weave.x - edge_dx,
-      weave.y - edge_dx,
-      weave.size + edge_dx,
-      weave.size + edge_dx
-      );
-
-    fill(weave.color);
-    // cross wide
-    rect(weave.x - edge_dx, weave.y, 
-      weave.size + edge_dx, weave.size / 2);
-    // cross tall
-    rect(weave.x, weave.y - edge_dx, 
-      weave.size /2, weave.size + edge_dx);
-  } 
-  if(weave_type == "highlight") {
-
-    // large background square
-    rect(weave.x - edge_dx,
-      weave.y - edge_dx,
-      weave.size + edge_dx,
-      weave.size + edge_dx
-      );
-
-    fill(weave.color);
-    rect(weave.x + edge_dx, weave.y - edge_dx, 
-      weave.size - edge_dx, weave.size / 4);
-  }
-  if(weave_type == "hat") {
-
-    // large background square
-    rect(weave.x - edge_dx,
-      weave.y - edge_dx,
-      weave.size + edge_dx,
-      weave.size + edge_dx
-      );
-
-    fill(weave.color);
-    rect(weave.x, weave.y - 2*edge_dx, 
-      weave.size - edge_dx, weave.size / 2);
-  }
-  if(weave_type == "angle") {
-
-    triangle(weave.x, weave.y,
-      weave.x + weave.size,  
-      weave.y,
-      weave.x + weave.size,  
-      weave.y + weave.size 
-       );
-    fill(weave.color);
-    triangle(weave.x, weave.y,
-      weave.x,  
-      weave.y + weave.size,
-      weave.x + weave.size,  
-      weave.y + weave.size 
-       );
-
-  }
-  if(weave_type == "dots") {
-    circle(weave.x, weave.y, 2,2);
-    circle(weave.x + weave.size, 
-      weave.y + weave.size, 2,2);
-    fill(weave.color);
-    circle(weave.x + weave.size, weave.y, 2,2);
-    circle(weave.x, 
-      weave.y + weave.size, 2,2);
-  }
-  if(weave_type == "offset_dots") {
-    circle(weave.x, weave.y, 2,2);
-    circle(weave.x + weave.size, 
-      weave.y + weave.size, 2,2);
-    fill(weave.color);
-    circle(weave.x + weave.size + 4, weave.y, 2,2);
-    circle(weave.x + 4, 
-      weave.y + weave.size, 2,2);
-  }
-  return weave;
+function draw_item(item) {
+  rect(item.x, item.y, 
+    item.size, 
+    item.size);
+  return item;
 }
 
-function anim_weave(weave) { 
+function anim_item(weave) { 
   weave.x += weave.dx;
   weave.y += weave.dy;
   return weave;
@@ -136,101 +57,34 @@ function zippy() {
     rect(x,0, stripe_w, 400);
   }
 }
-
-var weave_type = "";
-var weave_types = ["cross", 
-  "hat", "highlight", 
-  "angle", "dots", "offset_dots"];
 function setup() {
 
   setup_canvas(400,400);
-  background('#4444CC');
-  setup_grain();
-  starfield(400,400, 2);
   zippy();
+  var newb = Object.assign({}, item_template);
+  newb.x = 200; 
+  newb.y = 0;
+  items.push( 
+   newb 
+  );
 
+  fill(fresh_color());
   maxline = 400;
-  balls = [];
-  item_count = 4;
-  weave_type = choose(weave_types);
-
-  base_path = [];
-  fun_curves = [
-    [2,0,
-     2,0,
-      0,2,
-      ], // wide box
-    [2,0,
-      0,2,
-      -1,0,
-      0,-1,
-      -1,0,
-      0,2,
-      2,0,
-      ], // spiral
-    [0,2,
-      1,0,
-      0,-2
-      ] // U
-
-  ];
-  var curve_count = 4;
-  for(i=0; i<curve_count; i++) {
-    base_path = base_path.concat(
-      choose(fun_curves)
-    );
-  }
-  
-  latest_path = base_path;
-
-  weave_template = Object(weave);
-
-  startx = [330, 70, 330, 70];
-  starty = [330, 70, 130, 260];
-
-  for(i=0;i<item_count;i++){
-    newb = Object.assign({}, weave_template);
-
-    newb.x = startx[i];
-    newb.y = starty[i]; // Make sure they each start offset a bit
-
-
-    newb.color = fresh_color();
-    newb.color2 = fresh_color();
-
-    latest_path = latest_path.map( item => item * -1);
-    seq1 = latest_path;
-    seq2 = seq1.map( item => item * -1); // reverse to close loop
-    newb.sequence = newb.sequence.concat(seq1);
-    newb.sequence = newb.sequence.concat(seq2);
-
-    balls.push(newb);
-  }
-  noStroke();
-
-}
-
-function shift_weave(item) {
-  if(item.sequence.length > 0) {
-    item.dx = item.sequence.shift();
-    item.dy = item.sequence.shift();
-  } else {
-    item.dx = 0;
-    item.dy = 0;
-  }
-  return item;
 }
 
 function draw() {
+  /* 
+  
   if(pulse == 0) {
-    balls = balls.map( item => shift_weave(item) );
+    items = items.map( item => shift_item(item) );
   }
+  */
   pulse++;
   if(pulse > beat) {
     pulse = 0;
   }
-  balls = balls.map( item => anim_weave(item) );
-  balls = balls.map( item => draw_weave(item) );
+  items = items.map( item => anim_item(item) );
+  items = items.map( item => draw_item(item) );
 }
 
 function mouseClicked() {
--- js/art/share/canvas.js
+++ js/art/share/canvas.js
@@ -1,6 +1,18 @@
 
 function setup_canvas(maxim_x=800, maxim_y=400) {
   midline = maxim_x / 2;
+
   myCanvas = createCanvas(maxim_x, maxim_y);
 
+}
+
+function zippy() {
+  noStroke();
+  stripe_w = 60;
+  for(i=0; i<400; i++) {
+    fill(fresh_color(3)); // darker fill color
+    x = i * stripe_w;
+    // triangle(x, y, x+30, y-random(80), x, y+i+30);
+    rect(x,0, stripe_w, 400);
+  }
 }
