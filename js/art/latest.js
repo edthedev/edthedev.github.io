27369a4ce14853cbb7dbf1b7aac5f27478db1972
EdTheDev <edward@delaporte.us>
2021-12-24 22:18:08 -0600

    Rap out some BASIC commands.

--- js/art/latest.js
+++ js/art/latest.js
@@ -15,42 +15,85 @@ as long as you display this license and attribution.
 
 */
 
-var terms = ['LOADING', 'READY', 'RUN'];
+var terms = [
+  'LOADING', 
+  'READY.', 
+  'RUN',
+  '****',
+  '64 BASIC V2',
+  'SEARCHING FOR $',
+  'LIST',
+  '664 BLOCKS FREE.',
+  '64K RAM',
+  '38911 BASIC',
+  'BYTES FREE',
+  'LOAD "*", 8, 1',
+  '10 PRINT "HELLO"',
+  'INPUT A$',
+  '900 END',
+  'CLR',
+  'GOSUB 2000',
+  '20 GOTO 10',
+  'REM ART BY ED',
+];
 
-var grid_memory = [];
+var layout = [];
+/*
+var layout = [
+  [1,1,'LOADING'],
+  [1,2,'READY.'],
+  [1,3,'RUN'],
+];
+*/
+
 var grid_width = 32;
 var grid_size = grid_width * 40;
-var grid_y_start = 10;
+var grid_y_start = 13;
+var grid_x_start = 40;
 var grid_x_increment = 15;
 var grid_y_increment = 40;
 var sq_wide = 400 / grid_width;
 var sq_tall = 400 / grid_width;
 
-function draw_letter(letter, position) {
+function draw_letter(term, x, y) {
   fill('#B19EFF');
-  var x=(position % grid_width);
-  var y=int(position/grid_width);
-  console.debug("X: ", x);
-  console.debug("Y: ", y);
-  text(letter, x * grid_x_increment, 
+  text(term, x * grid_x_increment + grid_x_start, 
     y * grid_y_increment + grid_y_start);
-
 }
 
-function draw_grid() {
-  /*
-  for(var i=0; i<grid_memory.length; i++) {
-    draw_letter("?", i)
-  }*/
+function setup_layout() {
+  for(var i=0; i<grid_size; i++) {
+    var [x,y] = get_xy(i);
+    var next_term = choose(terms);
+    var new_row = [x, y, next_term];
+    layout.push(new_row);
+    i+= next_term.length;
+    i+= 5;
+  }
+}
+
+function get_xy(position) {
+  var x=(position % grid_width);
+  var y=int(position/grid_width);
+  return [x,y];
 }
 
 function update_grid(pulse) {
-  var tile = pulse % grid_size;
+  var position = pulse % grid_size;
+  var [x,y] = get_xy(position);
   /*
-  console.debug("Pulse: ", pulse);
-  console.debug("Tile: ", tile);
+  console.debug("X: ", x);
+  console.debug("Y: ", y);
   */
-  draw_letter("?", tile);
+  for(let i=0; i<layout.length; i++) {
+    var item = layout[i];
+    console.debug("item", item);
+    if(x==item[0] && 
+      y==item[1]
+      ) {
+        draw_letter(item[2], x,y);
+      }
+  }
 }
 
 
@@ -66,8 +109,9 @@ function setup() {
   textFont(font);
   textSize(30);
   textAlign(CENTER, CENTER);
+
+  setup_layout();
   
-  grid_memory = [];
   background('#6953F5');
   // starfield(400,400,4);
 }
@@ -81,7 +125,6 @@ function draw() {
   if(pulse % pulse_delay == 0) {
     update_grid(pulse / 10);
   }
-  draw_grid();
 }
 
 function mouseClicked() {
