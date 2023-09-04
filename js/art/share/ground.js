a51a6e439a1bfa521b777b656c5757dbb5d2cbed
EdTheDev <edward@delaporte.us>
2023-09-03 23:07:51 -0500

    Fixes

--- js/art/share/ground.js
+++ js/art/share/ground.js
@@ -1,8 +1,16 @@
+var season;
+var leaf_green;
+
+function setup_season() {
+  season = choose(["spring", "winter", "summer", "fall"]);
+  leaf_green = color(0, random(150,200), random(0,100));
+}
 
 function draw_grass(xi,y,x_end, y_end) {
 
   noStroke();
   dx = 1; // spiked grass
+
   fill(leaf_green); // summer
   if(season.includes("fall)")) {
     fill(color(20, 20, 30));
@@ -24,10 +32,13 @@ function draw_grass(xi,y,x_end, y_end) {
   }
 }
 
-function draw_ground(canv) {
+function draw_ground(canv, y=0) {
   maxim_x = canv.width;
   maxim_y = canv.height;
 
-  y = 4*maxim_y/5;
+  if(y==0) {
+    y = 4*maxim_y/5;
+  }
+
   draw_grass(0,y,maxim_x, maxim_y);
 }
