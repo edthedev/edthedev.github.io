f86e526f57e5d800cd13a88c8f4bd2ffda9c81b1
EdTheDev <edward@delaporte.us>
2023-09-07 22:15:13 -0500

    WIP mix and match

--- art/mix/index.md
+++ art/mix/index.md
@@ -1,8 +1,10 @@
 ---
 layout: liveart.multi.script.njk
 title: Mix and Match Sketches by Edward Delaporte
-script: ['/js/art/share/mix.js', '/js/art/share/random.js']
+script: ['/js/jquery.min.js', '/js/art/share/mix.js', '/js/art/share/random.js']
 ---
 
+# TODO: Update 11ty to provide a list of files in the directories
+# TODO: Clear errors.
 <img src="/img/mix/top/a.jpg" id=imgTop />
 <img src="/img/mix/top/a.jpg" id=imgBottom />
--- js/art/share/mix.js
+++ js/art/share/mix.js
@@ -1,9 +1,14 @@
 function get_random_img(path) {
+  alert(path);
   images = $.get(path);
   return random_choice(images);
 }
 
-window.onClick = function() {
-$("imgTop").src = get_random_img('/img/mix/top');
-$("imgBottom").src = get_random_img('/img/mix/bottom');
+alert('test');
+
+function updateMix() {
+  $("imgTop").src = get_random_img('/img/mix/top');
+  $("imgBottom").src = get_random_img('/img/mix/bottom');
 }
+
+document.getElementById("imgTop").addEventListener("load", updateMix);
