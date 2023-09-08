4ac72e9af65a6b08fceccad7fd192037a60ae6e8
EdTheDev <edward@delaporte.us>
2023-09-07 22:32:52 -0500

    Add mix-n-match list

---
+++ update_mix_json.ps1
@@ -0,0 +1,2 @@
+Get-ChildItem img/mix/top | ForEach-Object { $_.PsChildName } | ConvertTo-Json > _data/mix_top.json
+Get-ChildItem img/mix/bottom | ForEach-Object { $_.PsChildName } | ConvertTo-Json > _data/mix_bottom.json
