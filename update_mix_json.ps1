140a6dce36549e860ef6af90fe957df8240e9b45
EdTheDev <edward@delaporte.us>
2023-09-09 12:10:14 -0500

    Add Alt text helper

--- update_mix_json.ps1
+++ update_mix_json.ps1
@@ -1,2 +1,16 @@
 Get-ChildItem img/mix/top | ForEach-Object { $_.PsChildName } | ConvertTo-Json > data/mix_top.json
 Get-ChildItem img/mix/bottom | ForEach-Object { $_.PsChildName } | ConvertTo-Json > data/mix_bottom.json
+
+
+$names = Get-ChildItem img/mix/top | ForEach-Object { $_.PsChildName }
+$all_alt = Get-Content ./data/mix_alt.json | ConvertFrom-Json -AsHashTable
+
+$names | ForEach {
+        Write-Host $_
+    if(-Not $all_alt.keys -contains $_ ) {
+        $all_alt[$_] = "TODO: Add alt text"
+    }
+}
+
+Write-Output $all_alt
+$all_alt | ConvertTo-Json > ./data/mix_alt.json
