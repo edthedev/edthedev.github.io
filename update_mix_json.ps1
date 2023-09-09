b6cdbd6fad26b79b9cdd9a9f98ea4fffceed70ae
EdTheDev <edward@delaporte.us>
2023-09-09 12:25:56 -0500

    ALT text for bottom images in mix and match

--- update_mix_json.ps1
+++ update_mix_json.ps1
@@ -2,9 +2,19 @@ Get-ChildItem img/mix/top | ForEach-Object { $_.PsChildName } | ConvertTo-Json >
 Get-ChildItem img/mix/bottom | ForEach-Object { $_.PsChildName } | ConvertTo-Json > data/mix_bottom.json
 
 
-$names = Get-ChildItem img/mix/top | ForEach-Object { $_.PsChildName }
 $all_alt = Get-Content ./data/mix_alt.json | ConvertFrom-Json -AsHashTable
 
+# ALT text stubs for top images
+$names = Get-ChildItem img/mix/top | ForEach-Object { $_.PsChildName }
+$names | Sort-Object | ForEach {
+    if(-Not($all_alt.keys -Contains $_)) {
+        Write-Host "Add $_"
+        $all_alt[$_] = ""
+    }
+}
+
+# ALT text stubs for bottom images
+$names = Get-ChildItem img/mix/bottom | ForEach-Object { $_.PsChildName }
 $names | Sort-Object | ForEach {
     if(-Not($all_alt.keys -Contains $_)) {
         Write-Host "Add $_"
