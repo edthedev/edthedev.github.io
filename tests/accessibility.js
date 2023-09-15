848f27617107fb1949950ebda1d34b4775e89ecd
Edward Delaporte <edward@delaporte.us>
2023-09-15 17:17:39 +0100

    WIP - tests

---
+++ tests/accessibility.js
@@ -0,0 +1,28 @@
+const { AxeBuilder } = require('@axe-core/playwright');
+const playwright = require('playwright');
+
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://edward.delaporte.us/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle("Edward Delaporte's Site");
+});
+
+test('accessibility', async ({ page }) => {
+/* (async () => { */
+  // const browser = await playwright.chromium.launch({ headless: true });
+  // const context = await browser.newContext();
+  // const page = await context.newPage();
+  await page.goto('https://edward.delaporte.us/');
+
+  try {
+    const results = await new AxeBuilder({ page }).analyze();
+    console.log(results);
+  } catch (e) {
+    // do something with the error
+  }
+
+  await browser.close();
+});
