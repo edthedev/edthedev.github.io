df7d118b8a04ba878939724c85fa82c3edaa0f83
Edward Delaporte <edward@delaporte.us>
2023-09-15 19:20:39 +0100

    WIP: accessibility report artifact

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   timeout: 30000,
   globalTimeout: 600000,
-  reporter: 'list',
   testDir: './tests',
+  reporter: [['html', { open: 'never', outputFolder: 'pw-report'}]],
 });
