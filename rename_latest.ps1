59af32785f5a967daba874b02ef8dcbab70b27d2
EdTheDev <edward@delaporte.us>
2023-12-11 22:26:40 -0600

    Fixup

--- rename_latest.ps1
+++ rename_latest.ps1
@@ -8,8 +8,8 @@ param(
     [string]$name
 )
 
-Copy-Item ./img/art/latest.png./img/art/$name.PNG
-Copy-Item ./img/art/latest2.png /img/art/$($name)2.PNG
+Copy-Item ./img/art/latest.png ./img/art/$name.PNG
+Copy-Item ./img/art/latest2.png ./img/art/$($name)2.PNG
 Copy-Item ./js/art/latest.js ./js/art/$name.js
 Copy-Item ./art/live/latest.md ./art/live/$name.md
 (Get-Content ./art/live/$name.md) -replace 'latest', $name | Set-Content ./art/live/$name.md
