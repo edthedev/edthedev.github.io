b7b6bdc5c99a789c978644c74f078d1afd391214
EdTheDev <edward@delaporte.us>
2023-12-14 19:27:12 -0600

    Some code

---
+++ examples/pyxel/redlight/redlight.py
@@ -0,0 +1,52 @@
+
+import pyxel
+from dataclasses import dataclass
+
+
+@dataclass
+class Pixel:
+    x: int
+    y: int
+    color: int
+
+
+
+class App:
+    def __init__(self):
+        pyxel.init(200, 150, title="Red Light, Green Light")
+        pyxel.mouse(True)
+        pyxel.load('redlight.pyxres')
+
+        self.runner_x = 0
+        self.runner_y = 75
+
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        self.update_player()
+
+    def update_player(self):
+        if pyxel.btn(pyxel.KEY_LEFT) or pyxel.btn(pyxel.GAMEPAD1_BUTTON_DPAD_LEFT):
+            self.player_x = max(self.player_x - 2, 0)
+        if pyxel.btn(pyxel.KEY_RIGHT) or pyxel.btn(pyxel.GAMEPAD1_BUTTON_DPAD_RIGHT):
+            self.player_x = min(self.player_x + 2, pyxel.width - 16)
+        self.player_y += self.player_dy
+        self.player_dy = min(self.player_dy + 1, 8)
+
+    def draw(self):
+        pyxel.cls(6)  # Clear the screen to a light grey
+        pyxel.blt(0, 88, 0, 0, 88, 160, 32)
+        pyxel.blt(0, 88, 0, 0, 64, 160, 24, 12)
+
+        # Print text to tell us where the runner is
+        pyxel.text(x=3, y=3, s="runner is at: " + str(self.runner_x), col=0)
+        pyxel.text(x=120, y=140, s="Press ESC to quit.", col=0)
+
+        # Draw the runner
+        pyxel.blt(x=self.runner_x, y=self.runner_y, # draw at runner X,Y
+            img=0, # From image bank 0
+            u=0, v=0, w=16, h=16, # Choose pixels 0,0 through 16,16
+            colkey=0 # Treat color 0 as transparent
+            )
+
+App()
