70bd81702b5e968e4a8ddeaa0d22b72577caf96e
edthedev <edthedev@users.noreply.github.com>
2025-05-29 13:22:12 +0000

    deploy: 7395671dffc57978f1c58d451356c08c8936426a

---
+++ dotfiles/win/show-file-extensions.ps1
@@ -0,0 +1,11 @@
+function ShowFileExtensions()
+{
+    # http://superuser.com/questions/666891/script-to-set-hide-file-extensions
+    Push-Location
+    Set-Location HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced
+    Set-ItemProperty . HideFileExt "0"
+    Pop-Location
+    Stop-Process -processName: Explorer -force # This will restart the Explorer service to make this work.
+}
+ 
+ShowFileExtensions
