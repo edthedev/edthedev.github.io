103e86e72ab372ed3be02aee7163fefad31e7aa0
Edward Delaporte <edward@delaporte.us>
2026-07-31 16:01:52 -0500

    Resume pdf tools

--- resume/make-pdf.sh
+++ resume/make-pdf.sh
@@ -1,1 +1,1 @@
-$HOME/envs/docs/bin/md2pdf -c ../css/resume.css -i index.md -o Resume.Edward.Delaporte.pdf
+$HOME/envs/docs/bin/md2pdf -c ../css/resume-print.css -i index.md -o Resume.Edward.Delaporte.pdf
